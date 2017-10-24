69129ffc22ee33d2ee04952a9607d48045798fa7
dazzlegit <dronicl@yandex.ru>
2017-10-24 14:51:07 +0300

    lesson9 db config + index file

---
+++ source/data/index.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const dbConfig = require('../config/db');
+const logger = require('../../libs/logger')('DATABASE');
+
+const connect = async () => {
+  mongoose.Promise = global.Promise;
+  
+  const isTesting = process.env.NODE_ENV === 'testing';
+  const { host, port, name, options } = isTesting ? dbConfig.mongooseTest : dbConfig.mongoose;
+  const uri = `mongodb://${host}:${port}/${name}`;
+  
+  try {
+    await mongoose.connect(uri, options);
+    logger.info(`Mongoose is connected: ${uri}`);
+  } catch (err) {
+    logger.error(`Mongoose error: ${err}`);
+    process.exit(1);
+  }
+};
+
+const disconnect = async () => {
+  await mongoose.disconnect();
+  logger.info('Mongoose is disconnected');
+};
+
+module.exports = {
+  connect,
+  disconnect,
+};
